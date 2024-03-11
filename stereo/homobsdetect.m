b4d8ea374318033d5f1e0f24343fd3fad61b4575
Riccardo Persello <riccardo.persello@icloud.com>
2024-03-11 09:48:38 +0100

    Further cleanup.

--- stereo/homobsdetect.m
+++ stereo/homobsdetect.m
@@ -14,42 +14,6 @@ arguments(Output)
     I2 (:, :, :) uint8 {mustBeNonempty}
 end
 
-% Plane and fake camera.
-R = stereoParams.PoseCamera2.R;
-t = stereoParams.PoseCamera2.Translation;
-
-K1 = stereoParams.CameraParameters1.K;
-K2 = stereoParams.CameraParameters2.K;
-
-planeNormal = (t .^ (-1)) / 3 * (-inv(K2) * H.T * K1 + R);
-planeNormal = planeNormal ./ norm(planeNormal);
-
-% Let's build a rotation matrix so that the z-axis of the fake camera
-% points to the plane (opposite of its normal), and its y-axis (up) is
-% vertically aligned with the z-axis.
-
-% fakeCameraZ = [1 1 1];
-% fakeCameraY = (eul2rotm([pi/2 0 0]) * fakeCameraZ')';
-% fakeCameraX = (eul2rotm([0 pi/2 0]) * fakeCameraZ')';
-% 
-% fakeR = [fakeCameraX; fakeCameraY; fakeCameraZ];
-% 
-% quiver3([0 0 0], [0 0 0], [0 0 0], fakeR(:, 1)', fakeR(:, 2)', fakeR(:, 3)');
-% axis equal;
-% 
-% fakeCamera = cameraProjection(stereoParams.CameraParameters1.Intrinsics, rigidtform3d(fakeR, [0 0 0]));
-% 
-% figure(1);
-% plotCamera("AbsolutePose", fakePose);
-% hold on;
-% quiver3([0 0], [0 0], [0 0], [1 0], [0 0], [0 1]);
-% plot(planeModel([planeNormal 0]));
-% hold off;
-% axis equal;
-% 
-% fakeCameraHomography = K1 * (fakeCamera(1:3, 1:3) + (fakeCamera(:, end) * planeNormal) ./ d) * inv(K1);
-% fakeCameraHomography = projtform2d(fakeCameraHomography);
-
 % Undistort.
 I1 = undistortImage(I1, stereoParams.CameraParameters1);
 I2 = undistortImage(I2, stereoParams.CameraParameters2);
